44b5c50c6af3d1161821fa5aadf7421bda0273c5
sin392 <sin392@gmail.com>
2024-10-03 16:22:52 +0900

    refactor: 不要コメントの削除

--- sample/cmd/main.go
+++ sample/cmd/main.go
@@ -2,14 +2,6 @@ package main
 
 import "github.com/sin392/db-media-sample/sample/internal/app"
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-// @BasePath /v1
 func main() {
 	app, err := app.InitializeApplication()
 	if err != nil {
